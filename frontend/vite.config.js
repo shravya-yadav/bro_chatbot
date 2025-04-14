af7322030811c8f6a58760f593ac5913cb2f8498
shravya yadav <yadavshravya383@gmail.com>
2025-04-14 11:26:48 +0530

    Adding dependecies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/NetNudge",
+  base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
